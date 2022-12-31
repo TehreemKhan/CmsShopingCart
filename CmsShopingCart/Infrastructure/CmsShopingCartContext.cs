ec278b7bf0b90b7ac4e38cc11fa3c93ed0f44112
[Tehreem Khan] <[tehreem93@yahoo.com]>
2022-12-31 16:03:42 +0500

    db

---
+++ CmsShopingCart/Infrastructure/CmsShopingCartContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShopingCart.Infrastructure
+{
+    public class CmsShopingCartContext :DbContext
+    {
+        public CmsShopingCartContext(DbContextOptions<CmsShopingCartContext> options):base(options)
+        {
+
+        }
+    }
+}
