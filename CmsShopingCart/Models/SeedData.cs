875fab2ea98fededf65a367da00834f34614948f
[Tehreem Khan] <[tehreem93@yahoo.com]>
2022-12-31 17:51:47 +0500

    pages table seeded

---
+++ CmsShopingCart/Models/SeedData.cs
@@ -0,0 +1,52 @@
+﻿using CmsShopingCart.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShopingCart.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider) {
+            using (var context = new CmsShopingCartContext(serviceProvider.GetRequiredService<DbContextOptions<CmsShopingCartContext>>())) {
+                if (context.Pages.Any()) {
+                    return;
+                }
+                context.Pages.AddRange(
+                    new Page { 
+                        Title = "Home",
+                        Slug="home",
+                        Content="home page",
+                        Sorting=0
+                    },
+                    new Page
+                    {
+                        Title = "About us",
+                        Slug = "about-us",
+                        Content = "about us page",
+                        Sorting = 100
+                    },
+                    new Page
+                    {
+                        Title = "Service",
+                        Slug = "services",
+                        Content = "services page",
+                        Sorting = 3
+                    },
+                     new Page
+                     {
+                         Title = "Contact",
+                         Slug = "contact",
+                         Content = "contact page",
+                         Sorting = 4
+                     }
+                    );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- CmsShopingCart/Program.cs
+++ CmsShopingCart/Program.cs
@@ -1,5 +1,7 @@
+using CmsShopingCart.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +15,20 @@ namespace CmsShopingCart
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope()) {
+                var servies = scope.ServiceProvider;
+                try
+                {
+                    SeedData.Initialize(servies);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+                host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
