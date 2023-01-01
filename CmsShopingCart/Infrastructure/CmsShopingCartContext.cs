1613df485f289b5f029d385f161b63aa94fee0c1
[Tehreem Khan] <[tehreem93@yahoo.com]>
2023-01-01 17:09:19 +0500

    Category module added with create, edit , delete and sortable featres

--- CmsShopingCart/Infrastructure/CmsShopingCartContext.cs
+++ CmsShopingCart/Infrastructure/CmsShopingCartContext.cs
@@ -14,5 +14,6 @@ namespace CmsShopingCart.Infrastructure
 
         }
         public DbSet<Page> Pages { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
---
+++ CmsShopingCart/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShopingCart.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required, MinLength(3, ErrorMessage = "Minimun length is 3.")]
+        [RegularExpression(@"^[a-zA-Z-]+$", ErrorMessage = "Only letters are allowed.")]
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public int Sorting { get; set; }
+    }
+}
