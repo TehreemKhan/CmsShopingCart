bef8a1b0e6d5fb4ed4f5ca33b182941e2e3d3c65
[Tehreem Khan] <[tehreem93@yahoo.com]>
2023-01-01 15:47:26 +0500

    pages sortable feature dded

--- CmsShopingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShopingCart/Areas/Admin/Controllers/PagesController.cs
@@ -112,5 +112,20 @@ namespace CmsShopingCart.Areas.Admin.Controllers
             }
             return RedirectToAction("Index");
         }
+        //POST /admin/pages/reorder
+        [HttpPost]
+        public async Task<IActionResult> Reorder(int[]  id)
+        {
+            int count = 1;
+
+            foreach (var pageId in id) {
+                Page page = await context.Pages.FindAsync(pageId);
+                page.Sorting = count;
+                context.Update(page);
+                await context.SaveChangesAsync();
+                count++;
+            }
+            return Ok();
+        }
     }
 }
